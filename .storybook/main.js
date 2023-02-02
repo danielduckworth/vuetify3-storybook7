2f0c4147c23541f504ac0fbc9a76eb067866fae2
Domício Medeiros <domicio.medeiros@faro.com>
2023-02-02 16:44:39 +0000

    storybook 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,15 @@
-module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/vue3",
-  "core": {
-    "builder": "@storybook/builder-vite"
+const config = {
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  "framework": {
+    name: "@storybook/vue3-vite",
+    options: {}
   },
   "features": {
     "storyStoreV7": true
+  },
+  docs: {
+    autodocs: true
   }
-}
+};
+export default config;
